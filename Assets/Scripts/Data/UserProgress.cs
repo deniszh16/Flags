99fecd43e113b0b59f5b1e780c17618309d8622f
Denis Zhursky <zhursky16@gmail.com>
2024-02-14 22:57:16 +0300

    Локализация игры

--- Assets/Scripts/Data/UserProgress.cs
+++ Assets/Scripts/Data/UserProgress.cs
@@ -1,7 +1,34 @@
+using System;
+using UnityEngine;
+
 namespace Data
 {
+    [Serializable]
     public class UserProgress
     {
+        [SerializeField]
+        private int _progress;
+
+        public int Progress =>
+            _progress;
         
+        [SerializeField]
+        private int _locale;
+
+        public int Locale
+        {
+            get => _locale;
+            set => _locale = value;
+        }
+        
+        public bool Sound;
+
+        public UserProgress()
+        {
+            _progress = 1;
+        }
+
+        public void IncreaseProgress() =>
+            _progress++;
     }
 }
